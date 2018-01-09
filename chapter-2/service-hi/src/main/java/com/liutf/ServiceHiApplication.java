dec6efbeb4d4b83e1685e0748562891ab93a2e29
liutf <lautayfir@163.com>
2018-01-09 22:23:13 +0800

    del

--- chapter-2/service-hi/src/main/java/com/liutf/ServiceHiApplication.java
+++ chapter-2/service-hi/src/main/java/com/liutf/ServiceHiApplication.java
@@ -11,10 +11,10 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @EnableEurekaClient
 @RestController
-public class EurekaClientApplication {
+public class ServiceHiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EurekaClientApplication.class, args);
+		SpringApplication.run(ServiceHiApplication.class, args);
 	}
 
 	@Value("${server.port}")
