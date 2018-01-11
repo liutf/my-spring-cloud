df327f94ebd4c00a1065fd1cb407a2bae6ba844b
liutf <tengfei.liu@net263.com>
2018-01-11 11:49:21 +0800

    spring-cloud-config

---
+++ chapter-5/service-zuul/src/main/java/com/liutf/ServiceZuulApplication.java
@@ -0,0 +1,20 @@
+package com.liutf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableZuulProxy
+public class ServiceZuulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceZuulApplication.class, args);
+	}
+
+}
